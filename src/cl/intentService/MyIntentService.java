5ba5c954c3c394e5c80bb8c5d7d5b43f6860fe98
chenlong <751934754@qq.com>
2015-09-04 08:01:24 +0800

    IntentService案例

--- src/cl/intentService/MyIntentService.java
+++ src/cl/intentService/MyIntentService.java
@@ -3,7 +3,17 @@ package cl.intentService;
 import android.app.IntentService;
 import android.content.Intent;
 import android.util.Log;
-
+/**
+ * theory：
+ * IntentService will start a child thread automatically，
+ * and it's onHandleIntent method is on the child thread，
+ * when executed it will be destroyed。
+ * 
+ * use:
+ * to complete a internet's request or a time-consuming thing, 
+ * then use it can create child thread easily,
+ * because use it we don't consider about when to destroy the service and how to create a child thread.
+ */
 public class MyIntentService extends IntentService {
 
 	public MyIntentService() {
