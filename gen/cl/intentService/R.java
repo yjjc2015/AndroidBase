aa04f216d53e4fe9c3ba0a52d90004d49e7bac91
chenlong <751934754@qq.com>
2015-09-05 23:45:34 +0800

    RecycleView的学习

--- gen/cl/intentService/R.java
+++ gen/cl/intentService/R.java
@@ -10,29 +10,47 @@ package cl.intentService;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int blue=0x7f040002;
+        public static final int red=0x7f040001;
+        public static final int red_light=0x7f040000;
+        public static final int yellow=0x7f040003;
+    }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int divider_list=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int item_bg=0x7f020002;
     }
     public static final class id {
-        public static final int startIntentService=0x7f060001;
-        public static final int textView1=0x7f060007;
-        public static final int tv_httpclient=0x7f060000;
-        public static final int webView=0x7f060002;
-        public static final int webView1=0x7f060006;
-        public static final int wv1=0x7f060003;
-        public static final int wv2=0x7f060004;
-        public static final int wv3=0x7f060005;
+        public static final int btnAdd=0x7f070002;
+        public static final int btnDel=0x7f070003;
+        public static final int item_tv=0x7f070009;
+        public static final int recycleView=0x7f070004;
+        public static final int startIntentService=0x7f070001;
+        public static final int textView1=0x7f07000b;
+        public static final int tv_httpclient=0x7f070000;
+        public static final int webView=0x7f070005;
+        public static final int webView1=0x7f07000a;
+        public static final int wv1=0x7f070006;
+        public static final int wv2=0x7f070007;
+        public static final int wv3=0x7f070008;
     }
     public static final class layout {
         public static final int activity_httpclient=0x7f030000;
         public static final int activity_intentservice=0x7f030001;
-        public static final int activity_webview=0x7f030002;
-        public static final int activity_webview1=0x7f030003;
-        public static final int main=0x7f030004;
+        public static final int activity_recycleview=0x7f030002;
+        public static final int activity_webview=0x7f030003;
+        public static final int activity_webview1=0x7f030004;
+        public static final int item_hor_single_textview=0x7f030005;
+        public static final int item_single_textview=0x7f030006;
+        public static final int main=0x7f030007;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int startIntentService=0x7f040001;
+        public static final int action_gridview=0x7f050004;
+        public static final int action_listview=0x7f050003;
+        public static final int action_settings=0x7f050002;
+        public static final int app_name=0x7f050000;
+        public static final int startIntentService=0x7f050001;
     }
     public static final class style {
         /** 
@@ -40,26 +58,20 @@ public final class R {
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
     
 
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
-
         Base application theme for API 11+. This theme completely replaces
         AppBaseTheme from res/values/styles.xml on API 11+ devices.
     
- API 11 theme customizations can go here. 
 
         Base application theme for API 14+. This theme completely replaces
         AppBaseTheme from BOTH res/values/styles.xml and
         res/values-v11/styles.xml on API 14+ devices.
     
- API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
+        public static final int match=0x7f060002;
     }
 }
